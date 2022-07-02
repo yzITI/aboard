483e1038841c66994233274bab9d83640816c350
Phantomlsh <phantomlsh@outlook.com>
2022-07-01 20:18:51 -0700

    Fix count in children

--- controllers/block.js
+++ controllers/block.js
@@ -18,7 +18,7 @@ exports.get = async (_id, user) => {
   const res = {}
   for (const c of children) {
     res[c._id] = c
-    c.childrenCount = await B.count({ parent: c._id })
+    c.count = await B.count({ parent: c._id })
   }
   comet.send(user.id, wrap('block.children', _id, res))
 }
@@ -36,6 +36,7 @@ exports.put = async (block, user) => {
   block.author = user.name
   await B.put({ _id: block._id }, block)
   comet.pub(block._id, wrap('block.one', block))
+  block.count = await B.count({ parent: block._id })
   comet.pub(block.parent, wrap('block.children', block.parent, { [block._id]: block }))
 }
 
